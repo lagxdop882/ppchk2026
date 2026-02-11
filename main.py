c371a25d28335ed3b7e578df246d235e8f76aec1
lagxdop882 <155067717+lagxdop882@users.noreply.github.com>
2026-02-12 05:28:20 +0530

    Change bot token in main.py
    Updated the bot token for authentication.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 # Bot Configuration
-TOKEN = '8568309620:AAGxoy2C2od9v4w8VKskKRhSttlXCjXs1g4'
+TOKEN = '8505905087:AAGQtcaMqK2gHrFEKFGNEDbkK41Inz35hkM'
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
